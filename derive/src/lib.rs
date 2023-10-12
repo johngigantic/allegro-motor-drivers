09188a27956acfbe32a10c3d375795537d179704
John Little <johngigantic@gmail.com>
2023-10-12 11:47:04 -0700

    Add beginning of derive macro

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -4,19 +4,30 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(AllegroRegister)]
 pub fn derive(input: TokenStream) -> TokenStream {
+    let DeriveInput {
+        attrs,
+        vis,
+        ident,
+        generics,
+        data
+    } = parse_macro_input!(input as DeriveInput);
+
+    let bitsize = analyze_bitsize(attrs).unwrap();
+
     quote! {}.into()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+fn analyze_bitsize(attrs: Vec<syn::Attribute>) -> Option<u16> {
+    for attr in attrs {
+        if attr.path().is_ident("bitsize") {
+            let a: syn::LitInt = attr.parse_args().unwrap();
+            let value = a.base10_parse::<u16>().unwrap();
+            return Some(value);
+        }
     }
+    None
 }

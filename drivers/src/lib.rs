ff200cab4f60ac4b6fc19ff00f81ce824cb3adc8
John Little <johngigantic@gmail.com>
2023-10-15 17:32:29 -0700

    Fix two failing tests

--- drivers/src/lib.rs
+++ drivers/src/lib.rs
@@ -6,17 +6,17 @@ pub mod a49xx;
 pub mod amt49xxx;
 pub mod io;
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+// pub fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
